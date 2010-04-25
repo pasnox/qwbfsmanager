856dc0045ab42485d398104ac389a4cfd3fa2d40
pasnox <pasnox@78eea2df-e988-3a20-b539-5b14e1163657>
2010-04-25 11:17:09 +0000

    Add licensing headers in sources code.

--- qwbfs/src/qwbfsdriver/PartitionDiscHandle.h
+++ qwbfs/src/qwbfsdriver/PartitionDiscHandle.h
@@ -1,3 +1,23 @@
+/****************************************************************************
+**
+** 		Created using Monkey Studio v1.8.4.0b2 (1.8.4.0b2)
+** Authors   : Filipe AZEVEDO aka Nox P@sNox <pasnox@gmail.com>
+** Project   : qwbfs
+** FileName  : PartitionDiscHandle.h
+** Date      : 2010-04-25T13:05:33
+** License   : GPL
+** Home Page : http://code.google.com/p/qwbfs
+** Comment   : QWBFS Manager is a crossplatform WBFS Manager developed using Qt4/C++.
+** It's currently working under Unix/Linux, Mac OS X, and build under windows (but not yet working).
+** 
+** DISCLAIMER: THIS APPLICATION COMES WITH NO WARRANTY AT ALL, NEITHER EXPRESS NOR IMPLIED.
+** I DO NOT TAKE ANY RESPONSIBILITY FOR ANY DAMAGE TO YOUR WII CONSOLE OR WII PARTITION
+** BECAUSE OF IMPROPER USAGE OF THIS SOFTWARE.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
 #ifndef PARTITIONDISCHANDLE_H
 #define PARTITIONDISCHANDLE_H
 
