86d427557c932e75da6ca38a251d364bee59cd17
pasnox <pasnox@78eea2df-e988-3a20-b539-5b14e1163657>
2010-05-31 23:22:59 +0000

    Remove some warnings

--- qwbfs/src/qwbfsdriver/PartitionStatus.cpp
+++ qwbfs/src/qwbfsdriver/PartitionStatus.cpp
@@ -24,7 +24,7 @@ using namespace QWBFS::Partition;
 
 Status::Status( const QWBFS::Partition::Handle& handle )
 {
-	blocks = handle.isValid() ? wbfs_count_usedblocks( handle.ptr() ) : -1;
+	blocks = handle.isValid() ? (qint32)wbfs_count_usedblocks( handle.ptr() ) : -1;
 	size = handle.isValid() ? (double)handle.ptr()->n_wbfs_sec *handle.ptr()->wbfs_sec_sz : -1;
 	used = handle.isValid() ? (double)( handle.ptr()->n_wbfs_sec -blocks ) *handle.ptr()->wbfs_sec_sz : -1;
 	free = handle.isValid() ? (double)(blocks) *handle.ptr()->wbfs_sec_sz : -1;
