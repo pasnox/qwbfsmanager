e08adc405cfbb838f2636a67b82e7e64b8cb7717
pasnox <pasnox@78eea2df-e988-3a20-b539-5b14e1163657>
2011-03-16 08:38:33 +0000

    Some cleanup in mac partition model.

--- qwbfs/src/models/pPartitionModel_mac.mm
+++ qwbfs/src/models/pPartitionModel_mac.mm
@@ -19,14 +19,12 @@ http://developer.apple.com/library/mac/#samplecode/VolumeToBSDNode/Listings/Volu
 #ifndef IO_OBJECT_NULL
 #define IO_OBJECT_NULL  ((io_object_t)0)
 #endif
- 
+
 Boolean IsWholeMedia( io_service_t service )
 {
 	// Check that service is a IOMedia checking recursively the parents if needed and get its informations.
     Boolean isWholeMedia = false;
-    io_name_t className;
-    kern_return_t kernResult;
- 
+	
     if ( IOObjectConformsTo( service, kIOMediaClass ) ) {
         CFTypeRef wholeMedia = IORegistryEntryCreateCFProperty( service, CFSTR( kIOMediaWholeKey ), kCFAllocatorDefault, 0 );
 		
@@ -39,20 +37,142 @@ Boolean IsWholeMedia( io_service_t service )
         }
     }
 	
-    if ( isWholeMedia ) {
-        if ( IOObjectConformsTo( service, kIOCDMediaClass ) ) {
-            fprintf(stdout, "is a CD\n");
-        }
-        else if ( IOObjectConformsTo( service, kIODVDMediaClass ) ) {
-            fprintf(stdout, "is a DVD\n");
-        }
-        else {
-            kernResult = IOObjectGetClass(service, className);
-            fprintf(stdout, "is of class %s\n", className);
-        }            
-    }
- 
     return isWholeMedia;
+}
+
+void wholeMedia( io_service_t service, io_iterator_t iter, pPartitionModel::Partition& partition )
+{
+	Boolean isWhole;
+	io_name_t className;
+	
+	// A reference on the initial service object is released in the do-while loop below, so add a reference to balance
+	IOObjectRetain( service );
+	
+	partition.extendedAttributes[ "IS_CD" ] = "0";
+	partition.extendedAttributes[ "IS_DVD" ] = "0";
+	partition.extendedAttributes[ "IS_BLOCK" ] = "0";
+	
+	do {
+		isWhole = IsWholeMedia( service );
+		
+		if ( isWhole ) {
+			IOObjectGetClass( service, className );
+			
+			if ( IOObjectConformsTo( service, kIOCDMediaClass ) ) {
+				if ( partition.extendedAttributes[ "IS_CD" ] == "0" ) {
+					partition.extendedAttributes[ "IS_CD" ] = "1";
+				}
+			}
+			else if ( IOObjectConformsTo( service, kIODVDMediaClass ) ) {
+				if ( partition.extendedAttributes[ "IS_DVD" ] == "0" ) {
+					partition.extendedAttributes[ "IS_DVD" ] = "1";
+				}
+			}
+			else {
+				if ( partition.extendedAttributes[ "IS_BLOCK" ] == "0" ) {
+					partition.extendedAttributes[ "IS_BLOCK" ] = QString( "IOMedia" ) == className ? "1" : "0";
+				}
+			}
+			
+			partition.extendedAttributes[ "SERVICE_CLASSNAME" ] = className;
+		}
+		
+		IOObjectRelease( service );
+	} while ( ( service = IOIteratorNext( iter ) ) && !isWhole );
+}
+
+/*
+FSCopyDiskIDForVolume
+FSCopyURLForVolume
+*/
+
+void fillPartitionInformations( FSVolumeRefNum volume, pPartitionModel::Partition& partition )
+{
+	OSStatus result = noErr;
+	HFSUniStr255 volumeName;
+	FSVolumeInfo volumeInfo;
+	FSRef rootDirectory;
+	
+	bzero( (void*)&volumeInfo, sizeof(volumeInfo));
+	
+	if ( FSGetVolumeInfo( volume, 0, 0, kFSVolInfoGettableInfo, &volumeInfo, &volumeName, &rootDirectory ) != noErr ) {
+		return;
+	}
+	
+	GetVolParmsInfoBuffer volumeParms;
+	HParamBlockRec pb;
+	
+	pb.ioParam.ioNamePtr = 0;
+	pb.ioParam.ioVRefNum = volume;
+	pb.ioParam.ioBuffer = (Ptr)&volumeParms;
+	pb.ioParam.ioReqCount = sizeof(volumeParms);
+	
+	// A version 4 GetVolParmsInfoBuffer contains the BSD node name in the vMDeviceID field.
+	// It is actually a char* value. This is mentioned in the header CoreServices/CarbonCore/Files.h.
+	result = PBHGetVolParmsSync( &pb );
+	
+	if ( result != noErr ) {
+		qWarning() << QString( "PBHGetVolParmsSync returned %1." ).arg( result );
+		return;
+	}
+	else {
+		// currently i don't know how check network attached drives from mapping etc so let only use native volumes
+		if ( volumeParms.vMServerAdr != 0 ) {
+			return;
+		}
+		
+		// The volume is local if vMServerAdr is 0. Network volumes won't have a BSD node name.
+		if ( volumeParms.vMServerAdr == 0 ) {
+			CFMutableDictionaryRef matchingDict = IOBSDNameMatching( kIOMasterPortDefault, 0, (char*)volumeParms.vMDeviceID );
+			
+			if ( !matchingDict ) {
+				qWarning() << "IOBSDNameMatching returned a NULL dictionary.";
+			}
+			else {
+				io_service_t service = IOServiceGetMatchingService( kIOMasterPortDefault, matchingDict );    
+			
+				if ( service == IO_OBJECT_NULL ) {
+					qWarning() << "IOServiceGetMatchingService returned IO_OBJECT_NULL.";
+				}
+				else {
+					io_iterator_t iter;
+					kern_return_t kernResult = IORegistryEntryCreateIterator( service, kIOServicePlane, kIORegistryIterateRecursively | kIORegistryIterateParents, &iter );
+					
+					if ( kernResult != KERN_SUCCESS ) {
+						qWarning() << QString( "IORegistryEntryCreateIterator returned 0x%18x" ).arg( kernResult );
+					}
+					else if ( iter == IO_OBJECT_NULL ) {
+						qWarning() << "IORegistryEntryCreateIterator returned a NULL iterator.";
+					}
+					else {
+						wholeMedia( service, iter, partition );
+						IOObjectRelease( iter );
+					}
+					
+					IOObjectRelease( service );
+				}
+			}
+		}
+		
+		partition.label = QString::fromUtf16( volumeName.unicode, volumeName.length );
+		partition.origin = QString( "/dev/%1" ).arg( (char*)volumeParms.vMDeviceID );
+		partition.fileSystem = QString::null;
+		partition.total = volumeInfo.totalBytes;
+		partition.free = volumeInfo.freeBytes;
+		partition.used = partition.total -partition.free;
+		partition.fileSystem = volumeInfo.filesystemID == 0 ? "HFS/HFS+" : "";
+		partition.fileSystemMark = volumeInfo.filesystemID; // seem to be bad :(
+		partition.name = pPartitionModel::Partition::displayText(
+			partition.origin, 
+			partition.label, 
+			partition.fileSystemMark, 
+			QString::null, 
+			QString::null );
+		partition.extendedAttributes[ "REMOVABLE" ] = volumeParms.vMExtendedAttributes & bIsRemovable ? "1" : "0";
+		partition.lastCheck = QDateTime::currentDateTime();
+		
+		//qWarning() << partition.origin << partition.extendedAttributes;
+	}
 }
 
 pPartitionModel::Partitions pPartitionModel::partitions() const
@@ -60,107 +180,19 @@ pPartitionModel::Partitions pPartitionModel::partitions() const
 	pPartitionModel::Partitions partitions;
     OSStatus result = noErr;
     ItemCount volumeIndex;
- 
-    // Iterate across all mounted volumes using FSGetVolumeInfo. This will return nsvErr
-    // (no such volume) when volumeIndex becomes greater than the number of mounted volumes.
+	
     for ( volumeIndex = 1; result == noErr || result != nsvErr; volumeIndex++ ) {
-        FSVolumeRefNum actualVolume;
-        HFSUniStr255 volumeName;
-        FSVolumeInfo volumeInfo;
-		FSRef rootDirectory;
-        
-        bzero( (void*)&volumeInfo, sizeof(volumeInfo)) ;
-        
-		result = FSGetVolumeInfo( kFSInvalidVolumeRefNum, volumeIndex, &actualVolume, kFSVolInfoGettableInfo, &volumeInfo, &volumeName, &rootDirectory ); 
+        FSVolumeRefNum volume;
+		
+		result = FSGetVolumeInfo( kFSInvalidVolumeRefNum, volumeIndex, &volume, kFSVolInfoNone, 0, 0, 0 ); 
         
         if ( result == noErr ) {
-            GetVolParmsInfoBuffer volumeParms;
-            HParamBlockRec pb;
-            
-            pb.ioParam.ioNamePtr = NULL;
-            pb.ioParam.ioVRefNum = actualVolume;
-            pb.ioParam.ioBuffer = (Ptr)&volumeParms;
-            pb.ioParam.ioReqCount = sizeof(volumeParms);
-            
-            // A version 4 GetVolParmsInfoBuffer contains the BSD node name in the vMDeviceID field.
-            // It is actually a char * value. This is mentioned in the header CoreServices/CarbonCore/Files.h.
-            result = PBHGetVolParmsSync( &pb );
+			pPartitionModel::Partition partition;
+			fillPartitionInformations( volume, partition );
 			
-            if ( result != noErr ) {
-                qWarning() << QString( "PBHGetVolParmsSync returned %1." ).arg( result );
-            }
-            else {
-				if ( volumeParms.vMServerAdr != 0 ) { // currently i don't know how check network attached drives from mapping etc so let only use native volumes
-					continue;
-				}
-				
-				if ( volumeParms.vMServerAdr == 0 ) { // The volume is local if vMServerAdr is 0. Network volumes won't have a BSD node name.
-					CFMutableDictionaryRef matchingDict = IOBSDNameMatching( kIOMasterPortDefault, 0, (char*)volumeParms.vMDeviceID );
-					
-					if ( !matchingDict ) {
-						qWarning() << "IOBSDNameMatching returned a NULL dictionary.";
-					}
-					else {
-						io_service_t service = IOServiceGetMatchingService( kIOMasterPortDefault, matchingDict );    
-					
-						if ( service == IO_OBJECT_NULL ) {
-							qWarning() << "IOServiceGetMatchingService returned IO_OBJECT_NULL.";
-						}
-						else {
-							io_iterator_t iter;
-							kern_return_t kernResult = IORegistryEntryCreateIterator( service, kIOServicePlane, kIORegistryIterateRecursively | kIORegistryIterateParents, &iter );
-							
-							if ( kernResult != KERN_SUCCESS ) {
-								qWarning() << QString( "IORegistryEntryCreateIterator returned 0x%18x" ).arg( kernResult );
-							}
-							else if ( iter == IO_OBJECT_NULL ) {
-								qWarning() << "IORegistryEntryCreateIterator returned a NULL iterator.";
-							}
-							else {
-								Boolean isWholeMedia;
-								
-								// A reference on the initial service object is released in the do-while loop below, so add a reference to balance
-								IOObjectRetain( service );
-								
-								do {
-									isWholeMedia = IsWholeMedia( service );
-									IOObjectRelease( service );
-								} while ( ( service = IOIteratorNext( iter ) ) && !isWholeMedia );
-								
-								IOObjectRelease( iter );
-							}
-							
-							IOObjectRelease( service );
-						}
-					}
-				}
-				
-				/*HFSUniStr255 path;
-				result = FSGetCatalogInfo( &rootDirectory, kFSCatInfoNone, 0, &path, 0, 0 );
-				
-				qWarning() << "path" << QString::fromUtf16( path.unicode, path.length );*/
-				
-				pPartitionModel::Partition partition;
-		
-				partition.label = QString::fromUtf16( volumeName.unicode, volumeName.length );
-				partition.origin = QString( "/dev/%1" ).arg( (char*)volumeParms.vMDeviceID );
-				partition.fileSystem = QString::null;
-				partition.total = volumeInfo.totalBytes;
-				partition.free = volumeInfo.freeBytes;
-				partition.used = partition.total -partition.free;
-				partition.fileSystem = volumeInfo.filesystemID == 0 ? "HFS/HFS+" : "";
-				partition.fileSystemMark = volumeInfo.filesystemID; // seem to be bad :(
-				partition.name = pPartitionModel::Partition::displayText(
-					partition.origin, 
-					partition.label, 
-					partition.fileSystemMark, 
-					QString::null, 
-					QString::null );
-				partition.extendedAttributes[ "REMOVABLE" ] = volumeParms.vMExtendedAttributes & bIsRemovable ? "1" : "0";
-				partition.lastCheck = QDateTime::currentDateTime();
-				
+			if ( !partition.origin.isEmpty() ) {
 				partitions << partition;
-            }
+			}
         }
     }
 	
